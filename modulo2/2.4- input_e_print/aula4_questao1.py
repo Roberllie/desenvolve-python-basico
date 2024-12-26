dcc16fc0ceddd37a1c19dda5a27beb7f2fdf6fa0
Roberllie <160649185+Roberllie@users.noreply.github.com>
2024-12-26 16:17:09 -0300

    Update aula4_questao1.py

--- modulo2/2.4- input_e_print/aula4_questao1.py
+++ modulo2/2.4- input_e_print/aula4_questao1.py
@@ -1,13 +1,15 @@
-# input do terreno, comprimento e largura, e o preço por metro quadrado
-comprimento = int(input("Comprimento do terreno (m): "))
-largura = int(input("Largura do terreno (m): "))
-preco_metro2 = float(input("Preço do metro quadrado: "))
+# programa que calcula as dimen~soes e preço de um terreno
 
-# Calcular a área do terreno
-area_metro2 = comprimento * largura
+# solicita ao usuário e guarda os valores do terreno e o preço do metro quadrado
+comprimento = int(input("Comprimento: "))
+largura = int(input("Largura: "))
+preco_m2 = float(input("Preço do m/2: "))
 
-# Cálculo do preço total do terreno
-preco_total = preco_metro2 * area_metro2
+# calcula a área do terreno
+area_m2 = comprimento * largura
 
-# Impressão do resultado formatado
-print(f"O terreno possui {area_metro2}m2 e custa R${preco_total:,.2f}")
+# calculca preço total do terreno
+preco_total = area_m2 * preco_m2
+
+#Impressão formatada 
+print(f"O terreno possui {area_m2}m2 e custa {preco_total:,.2f} reais")
