f7ea895dfffa9dfef27facfdafbcfa7b82a76ed8
Roberllie <160649185+Roberllie@users.noreply.github.com>
2024-12-29 13:50:13 -0300

    Update aula2_questao1.py

--- Modulo3/3.2 Expressões-relacionais_e_logicas/aula2_questao1.py
+++ Modulo3/3.2 Expressões-relacionais_e_logicas/aula2_questao1.py
@@ -1,12 +1,10 @@
-juliana_idade = int(input("Informe a idade de Juliana: "))
-cris_idade = int(input("Informe a idade de Cris: "))
+# 1 - Juliana e Cris querem entrar em um bar, mas só podem se ambos forem maiores de idade (>17).
+# Escreva um programa que solicite as idades de Juliana e Cris e
+# imprima True se ambas forem maiores de 17 anos, indicando que podem entrar no bar, e False caso contrário.
 
-
-juliana_maior_de_idade = juliana_idade > 17
-cris_maior_de_idade = cris_idade > 17
-
-
-podem_entrar = juliana_maior_de_idade and cris_maior_de_idade
-
-
-print(podem_entrar)
+idade_juliana = int(input("Idade Juliana: "))
+idade_cris = int(input("Idade Cris: "))
+if (idade_juliana > 17 and idade_cris > 17):
+    print(True)
+else:
+    print(False)
