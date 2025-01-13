63a032bdc056d15e8b3de4bf81e65b810f7ac99e
Roberllie <160649185+Roberllie@users.noreply.github.com>
2025-01-13 11:18:05 -0300

    Update aula2_questao3.py

--- Modulo3/3.2 Expressões-relacionais_e_logicas/aula2_questao3.py
+++ Modulo3/3.2 Expressões-relacionais_e_logicas/aula2_questao3.py
@@ -1,18 +1,19 @@
-# Solicita a idade do usuário
-idade = int(input("Digite sua idade: "))
+#3 - Você está desenvolvendo um sistema de admissão para um clube juvenil de jogos de tabuleiro. 
+#Escreva um programa em Python que pergunte ao usuário sua idade, se já jogou pelo menos 3 jogos de tabuleiro 
+#(resposta deve ser True ou False) e quantas vezes venceu um jogo. O programa deve imprimir True se o 
+#participante tiver entre 16 e 18 anos, já tiver jogado pelo menos 3 jogos e já ter vencido pelo menos 1 jogo, 
+#permitindo seu ingresso no clube. Sua expressão deve imprimir False caso contrário. Aqui está um exemplo de 
+#interação com seu código no terminal, com entradas de dados destacadas em laranja e as impressões de seu 
+#código em branco.
 
-jogou_tres_jogos_str = input("Já jogou pelo menos 3 jogos de tabuleiro? (True/False): ")
-jogou_tres_jogos = jogou_tres_jogos_str == "True"
+idade = int(input("Idade: "))
+qtd_jogos = input("Já jogou 3 ou mais jogos: (t/f)")
+vitorias = int(input("Quantas vezes venceu: "))
+ingresso = False 
 
+if (idade <= 18 and idade >= 16):
+    if (qtd_jogos): #a comparação dessa variavel não pode ser com numeros, mas em booleanos (existe ou não)
+        if (vitorias >= 1):
+            ingresso = True
 
-vitorias = int(input("Quantos jogos já venceu? "))
-
-idade_valida = 16 <= idade <= 18
-
-jogos_validos = jogou_tres_jogos
-
-vitorias_validas = vitorias > 0
-
-apto = idade_valida and jogos_validos and vitorias_validas
-
-print(f"Apto para ingressar no clube de jogos de tabuleiro: {apto}")
+print(f"Ingresso: {ingresso}")
