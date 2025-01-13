6312745b75553dfdf984084b56fd2e47a315bc24
Roberllie <160649185+Roberllie@users.noreply.github.com>
2025-01-13 09:40:36 -0300

    Update aula4_questao4.py

--- modulo2/2.4- input_e_print/aula4_questao4.py
+++ modulo2/2.4- input_e_print/aula4_questao4.py
@@ -1,38 +1,54 @@
-# Solicita ao usuário o valor em reais
-valor = int(input("Informe um valor em reais: "))
+ #Escreva um programa que leia um valor inteiro referente a uma quantia em reais e calcule a menor quantidade 
+ #possível de notas necessárias para pagar aquele valor. As notas possíveis são: 100, 50, 20, 10, 5, 2, 1. 
+ #A saída deve estar formatada exatamente como indicado.
 
-# Calcula a quantidade de notas de 100 reais necessárias
-notas_100 = valor // 100
-valor %= 100
+saque = int(input("Valor do saque desejado: "))
+cont_100 = 0
+cont_50 = 0
+cont_20 = 0
+cont_10 = 0 
+cont_5 = 0 
+cont_2 = 0
+cont_1 = 0 
 
-# Calcula a quantidade de notas de 50 reais necessárias
-notas_50 = valor // 50
-valor %= 50
+while (saque >= 100):
+    saque // 100
+    cont_100 = cont_100 + 1
+    saque = saque - 100
+print (f"100: {cont_100}")
 
-# Calcula a quantidade de notas de 20 reais necessárias
-notas_20 = valor // 20
-valor %= 20
+while (saque >= 50):
+    saque // 50
+    cont_50 = cont_50 + 1
+    saque = saque - 50
+print (f"50: {cont_50}")
 
-# Calcula a quantidade de notas de 10 reais necessárias
-notas_10 = valor // 10
-valor %= 10
+while (saque >= 20):
+    saque // 20
+    cont_20 = cont_20 + 1
+    saque = saque - 20
+print (f"20: {cont_20}")
 
-# Calcula a quantidade de notas de 5 reais necessárias
-notas_5 = valor // 5
-valor %= 5
+while (saque >= 10):
+    saque // 10
+    cont_10 = cont_10 + 1
+    saque = saque - 10
+print (f"10: {cont_10}")
 
-# Calcula a quantidade de notas de 2 reais necessárias
-notas_2 = valor // 2
-valor %= 2
+while (saque >= 5):
+    saque // 5
+    cont_5 = cont_5 + 1
+    saque = saque - 5
+print (f"5: {cont_5}")
 
-# Calcula a quantidade de notas de 1 real necessárias
-notas_1 = valor // 1
+while (saque >= 2):
+    saque // 2
+    cont_2 = cont_2 + 1
+    saque = saque - 2
+print (f"2: {cont_2}")
 
-# Exibe o resultado formatado
-print(f"{notas_100} nota(s) de R$100,00")
-print(f"{notas_50} nota(s) de R$50,00")
-print(f"{notas_20} nota(s) de R$20,00")
-print(f"{notas_10} nota(s) de R$10,00")
-print(f"{notas_5} nota(s) de R$5,00")
-print(f"{notas_2} nota(s) de R$2,00")
-print(f"{notas_1} nota(s) de R$1,00")
+while (saque >= 1):
+    saque // 1
+    cont_50 = cont_100 + 1
+    saque = saque - 1
+print (f"1: {cont_1}")
