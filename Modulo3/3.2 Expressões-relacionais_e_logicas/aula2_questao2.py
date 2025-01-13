721165306eaa15eae2f87951d3910a54d49edf7b
Roberllie <160649185+Roberllie@users.noreply.github.com>
2025-01-13 10:56:34 -0300

    Update aula2_questao2.py

--- Modulo3/3.2 Expressões-relacionais_e_logicas/aula2_questao2.py
+++ Modulo3/3.2 Expressões-relacionais_e_logicas/aula2_questao2.py
@@ -1,11 +1,14 @@
-#2 - Dando continuidade à questão anterior, um outro bar permite a entrada de grupos onde pelo menos 
-# uma pessoa é maior de idade (ficando responsável pelas outras). Ajuste sua resposta da questão anterior, 
-# ainda solicitando as idades de Juliana e Cris, mas a
-# justando a expressão para esse novo cenário, imprimindo True se puderem entrar no bar, e False caso contrário.
+#2 - Dando continuidade à questão anterior, um outro bar permite a entrada de grupos onde pelo menos uma pessoa 
+#é maior de idade (ficando responsável pelas outras). Ajuste sua resposta da questão anterior, ainda solicitando
+#as idades de Juliana e Cris, mas ajustando a expressão para esse novo cenário, imprimindo True se puderem 
+#entrar no bar, e False caso contrário.
 
-idade_juliana = int(input("Idade Juliana: "))
-idade_cris = int(input("Idade Cris: "))
-if (idade_juliana > 17 or idade_cris > 17):
-    print(True)
+Juliana = int(input("Idade juliana: "))
+Cris = int(input("Idade Cris: "))
+podem = False 
+if (Juliana >17 or Cris >17):
+    podem = True
 else:
-    print(False)
+    podem = False 
+
+print(podem)
