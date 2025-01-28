4fc6a61f3862026023af6c6b58d327ae71f59299
Roberllie <160649185+Roberllie@users.noreply.github.com>
2025-01-28 19:30:27 -0300

    Update aula4_questao1.py

--- Modulo3/3.4 Condicionais/aula4_questao1.py
+++ Modulo3/3.4 Condicionais/aula4_questao1.py
@@ -1,11 +1,11 @@
+#Escreva um programa que lê dois números e informa se a sua soma é par ou ímpar. Critério: se o resto da 
+# divisão do número por 2 for 0, o número é par, caso contrário é ímpar. Lembre-se do operador do python % 
+# que retorna o resto de uma divisão. 
 
-num1 = int(input("Digite o primeiro número: "))
-num2 = int(input("Digite o segundo número: "))
+num1 = int(input("Digite um número: "))
+num2 = int(input("Outro: "))
 
-soma = num1 + num2
-
-if soma % 2 == 0:
-    print("A soma é par.")
-else:
-    print("A soma é ímpar.")
+soma = num1 + num2 
+div_2 = soma % 2 
+print ("par" if div_2 == 0 else "impar"  )
 
