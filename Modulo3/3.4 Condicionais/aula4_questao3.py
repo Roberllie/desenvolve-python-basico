166b188c11877d75fa2dc60d8a5bcb5116880a34
Roberllie <160649185+Roberllie@users.noreply.github.com>
2025-01-29 18:34:22 -0300

    Update aula4_questao3.py

--- Modulo3/3.4 Condicionais/aula4_questao3.py
+++ Modulo3/3.4 Condicionais/aula4_questao3.py
@@ -1,6 +1,11 @@
-ano = int(input("Insira um ano: "))
+#Você está desenvolvendo um programa para verificar se um ano é bissexto. Escreva um código em Python 
+# que solicita ao usuário para inserir um ano e imprime "Bissexto" se o ano for (1) divisível por 4 e 
+# não for divisível por 100, ou (2) se for divisível por 400. Caso contrário, imprima "Não Bissexto". 
 
-if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+ano = int(input("Digite um ano: "))
+if (ano % 4 == 0 and ano % 100 != 0):
+    print("Bissexto")
+elif (ano % 400 == 0):
     print("Bissexto")
 else:
-    print("Não Bissexto")
+    print("Não bissexto")
