a12762262fd444485cf58ef0c284fe94af369bef
Roberta Araujo <160649185+Roberllie@users.noreply.github.com>
2025-02-07 11:43:07 -0300

    Update aula1_questao3.py

--- modulo5/aula1_questao3.py
+++ modulo5/aula1_questao3.py
@@ -1,15 +1,12 @@
 import random
 
-numero_aleatorio = random.randint(1, 10)
-
-palpite = None
-
-while palpite != numero_aleatorio:
+num = random.randint(1,10)
+while(True):
     palpite = int(input("Adivinhe o número entre 1 e 10: "))
-
-    if palpite > numero_aleatorio:
+    if (palpite == num):
+        print(f"Correto! O número é {num}")
+        break
+    if (palpite > num):
         print("Muito alto, tente novamente!")
-    elif palpite < numero_aleatorio:
-        print("Muito baixo, tente novamente!")
     else:
-        print(f"Correto! O número é {numero_aleatorio}.")
+        print("Muito baixo, tente novamente!")
