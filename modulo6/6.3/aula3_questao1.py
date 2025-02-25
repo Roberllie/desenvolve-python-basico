982c06f7d5b48c1aeedb2f7c4d6f3a1de257ac5b
Roberta Araujo <160649185+Roberllie@users.noreply.github.com>
2025-02-25 13:15:57 -0300

    Update aula3_questao1.py

--- modulo6/6.3/aula3_questao1.py
+++ modulo6/6.3/aula3_questao1.py
@@ -1,19 +1,14 @@
-numeros = []
-while len(numeros) < 4:
-    try:
-        num = int(input("Digite um número inteiro: "))
-        numeros.append(num)
-    except ValueError:
-        print("Valor inválido. Digite apenas números inteiros.")
+import random 
 
-print("Lista original:", numeros)
+tan = int(input("Tamanho da lista igual ou acima de 4: "))
+lista = []
+for i in range(4):
+    
+    lista.append(random.randint(0,100))
 
-print("Os 3 primeiros elementos:", numeros[:3])
-
-print("Os 2 últimos elementos:", numeros[-2:])
-
-print("Lista invertida:", numeros[::-1])
-
-print("Elementos de índice par:", numeros[::2])
-
-print("Elementos de índice ímpar:", numeros[1::2])
+print(lista)
+print(lista[0:3])
+print(lista[:-3:-1])
+print(lista[::-1])
+print(lista[0:tan:2])
+print(lista[1:tan:2])
