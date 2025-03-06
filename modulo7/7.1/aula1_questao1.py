d14f7d18ebb37ddc126723a4321c505662da269a
Roberta Araujo <160649185+Roberllie@users.noreply.github.com>
2025-03-06 10:57:10 -0300

    Update aula1_questao1.py

--- modulo7/7.1/aula1_questao1.py
+++ modulo7/7.1/aula1_questao1.py
@@ -1,3 +1,4 @@
+#Escreva um programa que solicita o nome do usuário e o imprime em forma de escada
 nome = input("Digite seu nome: ")
 
 letras = ""
@@ -5,3 +6,9 @@ letras = ""
 for letra in nome:
     letras+= letra 
     print(letras)
+##outra alternativa 
+
+nome = input("Digite o seu nome: ")
+
+for i in range(len(nome)):
+    print(nome[:i])
