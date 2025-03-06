d229ea376f0862fe9b947ec08ce3fc9e4b6c56e0
Roberta Araujo <160649185+Roberllie@users.noreply.github.com>
2025-03-06 12:03:49 -0300

    Update aula1_questao4.py

--- modulo7/7.1/aula1_questao4.py
+++ modulo7/7.1/aula1_questao4.py
@@ -1,17 +1,6 @@
-numero = input("Digite o número: ")
-
-numero = numero.replace(" ", "").replace(".", "").replace("-", "")
-
+numero = input("Digite um número: ")
 if len(numero) == 8:
-    numero_completo = f"9{numero[:4]}-{numero[4:]}"
-elif len(numero) == 9:
-    if numero[0] == '9':
-        numero_completo = f"{numero[:5]}-{numero[5:]}"
-    else:
-        print("Número inválido.")
-        exit()
-else:
-    print("Número inválido.")
-    exit()
-
-print(f"Número completo: {numero_completo}")
+    numero = '9'+numero 
+if len(numero) == 9:
+        num_sepa = numero[:5]+"-"+numero[5:]
+        print(num_sepa)
